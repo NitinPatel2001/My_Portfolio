916081a7802ba471d5b68f309ec271ce10e554e5
NitinPatel2001 <nitinpatel_co20b5_40@dtu.ac.in>
2024-01-12 17:56:49 +0530

    resume

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <img className='navlogo' alt='cat' src='https://www.freeiconspng.com/thumbs/penguin-png/madagascar-penguin-png-1.png'></img>
         <nav className='navitem'>
           <a alt='Project' href='#Project'>My Work</a>
-          <a alt='Project' href='https://drive.google.com/file/d/1th2jauif4lhmmIcr9rTq4y0mAES3sw1G/view?usp=drive_link'>Resume</a>
+          <a alt='Project' href='https://drive.google.com/file/d/1QS-LnUGlG_Si4fuPi-PWWd20mbjQG9HE/view?usp=sharing'>Resume</a>
           {/* <a alt='Project' href='.Project'>Contact</a> */}
           <WbSunnyIcon/>
         </nav>
