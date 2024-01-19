2652181d82e32ac6c2216ee819bc4ff38aaf6b7b
NitinPatel2001 <nitinpatel_co20b5_40@dtu.ac.in>
2024-01-20 01:04:59 +0530

    Remove 1 error

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Navbar.css'
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -13,7 +12,6 @@ const Navbar = () => {
           <Link alt='Project' to='/Experience'>Experience</Link>
           <Link alt='Project' to='/Project'>My Work</Link>
           <a alt='Project' href='https://drive.google.com/file/d/1QS-LnUGlG_Si4fuPi-PWWd20mbjQG9HE/view?usp=sharing'>Resume</a>
-          {/* <WbSunnyIcon/> */}
         </nav>
       </div>
     </div>
